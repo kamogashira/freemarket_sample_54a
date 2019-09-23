218bc519f3c0aff651d472bf0ad34ac516d79203
kamogashira <asuka.kamo@icloud.com>
2019-09-24 08:13:39 +0900

    ヘッダーとスライドバナーの調整
    ・ヘッダーのプルダウンメニュー表示の修正
    ・slickを使用したスライドバナーの調整（途中）

--- app/assets/javascripts/top_page_banner.js
+++ app/assets/javascripts/top_page_banner.js
@@ -2,6 +2,7 @@ $(document).on('turbolinks:load', function(){
   $('#slide').slick({
     autoplay:true,
     dots:true,
-    arrows: true
+    arrows: true,
+    // prevArrow: '<div class="slide-arrow prev-arrow"><img src="top_page/arrow-left.png"></div>'
   });
 });
