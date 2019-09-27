1e7aef6c42d04f421e970cca7f6f5271336e099a
kamogashira <asuka.kamo@icloud.com>
2019-09-27 14:56:11 +0900

    プロフィール画面のマークアップ
    ・サイドバーは部分テンプレート_side_bar.scssに切り分け
    ・user controllerに空のeditアクション定義
    ・users/editのルーティングを仮で設定
    ・トップページスライドバナーの .slide-banner-phone__topクラスで使用している背景画像を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     
   end
 
+  def edit
+    @user = ""
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'products#index'
+  get 'users/edit' => 'users#edit'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'users' => 'users#index'
