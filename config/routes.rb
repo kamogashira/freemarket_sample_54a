436b623d0fa311f294316e2f399dbad5037ad164
Atsushi-Endo <ena-choco@live.jp>
2019-10-05 23:45:41 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'products#index'
-  get 'users' => 'users#index'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'users' => 'users#index'
