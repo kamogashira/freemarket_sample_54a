014bb901f9e4fd450b69055fde26d774d31985bd
Atsushi-Endo <ena-choco@live.jp>
2019-10-06 12:16:52 +0900

    とりあえずindexに飛ぶように変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'products#index'
-  resources :users
+  resources :users, action: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'users' => 'users#index'
