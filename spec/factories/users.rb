c6b773343cf81269d62fd8d79ba1ee7e96611d9d
ohirofumi <o_hiro975@yahoo.co.jp>
2019-10-24 22:44:22 +0900

    コンフリクトの修正

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-
-    factory :user do
-      nickname              {"abe"}
-      email                 {"kkk@gmail.com"}
-      password              {"00000000"}
-    end
-  
-  end
