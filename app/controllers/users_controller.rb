4fbebd4d29349276090ae572b2f82260407ed72a
kamogashira <asuka.kamo@icloud.com>
2019-11-14 21:24:47 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,12 @@ class UsersController < ApplicationController
   end
 
   def selling
-    @products = Product.where('seller_id = ? and current_status = ?', @user.id, 0)
+    @products = Product.where('seller_id = ? and current_status = ?', current_user.id, 0)
     @status = "出品中"
   end
 
   def trading
-    @products = Product.where('seller_id = ? and current_status = ?', @user.id, 1)
+    @products = Product.where('seller_id = ? and current_status = ?', current_user.id, 1)
     @products.each do |product|
       if ( product.payment_status == 1 && product.sending_status == 0 )
         @status = "発送待ち" 
