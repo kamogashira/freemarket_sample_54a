204bcc564236fc3b89a7a57d2550af5e3cc53360
kamogashira <asuka.kamo@icloud.com>
2019-11-22 11:25:13 +0900

    Update product_image_test.rb

--- db/seeds/product_image_test.rb
+++ db/seeds/product_image_test.rb
@@ -1,5 +1,5 @@
 #テスト用product_imageデータ作成
-1.upto(8) do |n|
+1.upto(9) do |n|
   ProductImage.create!(product_id: n,
                image: open("#{Rails.root}/db/fixtures/img#{n}.jpg")
               )
