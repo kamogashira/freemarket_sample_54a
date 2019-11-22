6add3465c90a5c60f2bd37860bfdd88922d19a98
Atsushi-Endo <ena-choco@live.jp>
2019-11-22 10:26:31 +0900

    出品した商品を削除できるように変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :product_images
+  has_many :product_images, dependent: :destroy
   accepts_nested_attributes_for :product_images
 
   belongs_to :category
