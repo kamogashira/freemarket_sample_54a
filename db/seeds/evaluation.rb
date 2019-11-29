eec85776f34ec27be1edd62931ae1269e8fe3ea4
ohirofumi <o_hiro975@yahoo.co.jp>
2019-11-30 00:39:10 +0900

    seed追加

--- db/seeds/evaluation.rb
+++ db/seeds/evaluation.rb
@@ -1,6 +1,6 @@
 #テスト用evaluationデータ作成
 Evaluation.create!(user_id: 1,
-                   product_id: 1,
+                   product_id: 22,
                    good: 3,
                    normal: 2,
                    bad: 1,
